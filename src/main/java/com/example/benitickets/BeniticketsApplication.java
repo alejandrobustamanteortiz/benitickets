4ae4fab68b3a4970252ea49bd6995f7ed66ad0da
bububuu <albusor@gmail.com>
2023-03-16 00:55:41 +0100

    Initial commit

---
+++ src/main/java/com/example/benitickets/BeniticketsApplication.java
@@ -0,0 +1,13 @@
+package com.example.benitickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeniticketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeniticketsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/benitickets/BeniticketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.benitickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeniticketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
