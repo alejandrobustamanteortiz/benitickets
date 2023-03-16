7f322005243c0a7f385989eb18442c20b32c5943
bububuu <albusor@gmail.com>
2023-03-16 02:48:48 +0100

    Initial add bootstrap & webjars

--- src/main/java/com/example/benitickets/App.java
+++ src/main/java/com/example/benitickets/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BeniticketsApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(BeniticketsApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
