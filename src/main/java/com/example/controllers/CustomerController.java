ed0908735a91ccd0ec9d0bdb9e323e5f4b2ae3ad
bububuu <albusor@gmail.com>
2023-03-23 03:15:37 +0100

    SpringWeb - Customer-detail & Customer-list

--- src/main/java/com/example/controllers/CustomerController.java
+++ src/main/java/com/example/controllers/CustomerController.java
@@ -1,10 +1,14 @@
 package com.example.controllers;
 
+import com.example.entities.Customer;
 import com.example.services.CustomerService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
@@ -16,5 +20,25 @@ public class CustomerController {
     public String findAll(Model model) {
         model.addAttribute("customers", customerService.findAll());
         return "customer/customer-list";
+    }
+
+    @GetMapping("/customers/{id}")
+    public String findById(Model model, @PathVariable Long id){
+        Optional<Customer> customerOptional = customerService.findById(id);
+
+        if(customerOptional.isPresent()){
+            model.addAttribute("customerId" , customerOptional.get());
+        }else {
+            model.addAttribute("error", "Not found");
+        }
+        return "customer/customer-detail";
+    }
+
+    @GetMapping("/customers/{id}/delete")
+    public String deleteById(@PathVariable Long id){
+
+        customerService.deleteById(id);
+
+        return "redirect:/customers";
     }
 }
